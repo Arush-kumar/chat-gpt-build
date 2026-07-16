166f39464ac06f9b7786543ab9c26f8707fcf08b
Arush Kumar <arushkumarbjn123@gmail.com>
2026-07-16 17:53:40 +0530

    03 Authentication with Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="">
       <h1>Hello App</h1>
       <ModeToggle />
+      <UserButton />
     </div>
   );
 }
